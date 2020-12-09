fd2504c2d68a20e639e38a6a22390fd3719e0521
Prince <prince100thomas@gmail.com>
2020-12-09 16:18:13 +0530

    Angram algo completed

---
+++ anagrams.js
@@ -0,0 +1,21 @@
+function countingAnagrams(string) {
+    if (string === undefined)
+        throw new Error("Provide a string")
+    const words = string.split(' ').filter(value => value.length > 1)
+
+    const sorted_words = words.map(value => value.split("").sort().join("")).sort()
+    let i = 0;
+    let anagram_count = 0;
+    while (i < sorted_words.length - 2) {
+        if(sorted_words[i] == sorted_words[i+1]){
+            anagram_count++
+            i=sorted_words.lastIndexOf(sorted_words[i])
+        }else
+            i++
+    }
+
+    return anagram_count
+}
+
+
+module.exports = countingAnagrams
